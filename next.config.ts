c4570a194b1b439633c5a9b0af959e59d13fe551
unknown <eternalism@naver.com>
2026-01-17 04:21:22 +0900

    약간 수정

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		localPatterns: [],
+		localPatterns: [
+			{ pathname: "/api/image/**" }
+		],
 		remotePatterns: [
 			{ protocol: "http", hostname: "localhost" },
 			{ protocol: "https", hostname: "book-photo-cms.eternalism.workers.dev" }
